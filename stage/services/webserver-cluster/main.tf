12d70eff1dc88c318af38000c56507b8d0ee629f
cprinos <cprinos@chewy.com>
2019-01-03 13:32:19 -0500

    last changes for chapter 3... moved html code to external template file

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -11,23 +11,27 @@ data "terraform_remote_state" "db" {
         region = "us-east-1"
     }
 }
+
+data "template_file" "user_data" {
+    template = "${file("user-data.sh")}"
+
+    vars {
+        server_port = "${var.server_port}"
+        db_address = "${data.terraform_remote_state.db.address}"
+        db_port = "${data.terraform_remote_state.db.port}"
+    }
+}
 resource "aws_launch_configuration" "example-asg" {
     image_id = "ami-40d28157"
     instance_type = "t2.micro"
     security_groups = ["${aws_security_group.instance.id}"]
 
-   user_data = <<-EOF
-        #!/bin/bash
-        echo "Hello, world?" > index.html
-        echo "${data.terraform_remote_state.db.address}" >> index.html
-        echo "${data.terraform_remote_state.db.port}" >> index.html
-        echo "test" >> index.html
-        nohup busybox httpd -f -p "${var.server_port}" &
-        EOF
-
+    user_data = "${data.template_file.user_data.rendered}"
+    
     lifecycle {
         create_before_destroy = true
     }
+    name_prefix = "tf-book-"
 
 }
 
@@ -45,6 +49,7 @@ resource "aws_autoscaling_group" "example" {
         value = "terraform-asg-example"
         propagate_at_launch = true
     }
+    name_prefix = "terrabook-asg-prefix"
 
 }
 
